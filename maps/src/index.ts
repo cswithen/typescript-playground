ca9344aa1123f8a329a063da22f3968be64aef7e
Cody Swithenbank <cody.swithenbank@gmail.com>
2023-02-02 10:11:50 -0500

    feat(mapsInstance): added googleMaps instance

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,9 +1,10 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
-
 const company = new Company();
 
-console.log(user);
-console.log(company);
+new CustomMap("map");
