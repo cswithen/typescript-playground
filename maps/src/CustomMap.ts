dc6b629f63b1a414635bccb5387584471d06b3f1
Cody Swithenbank <cody.swithenbank@gmail.com>
2023-02-02 10:48:01 -0500

    feat(interface): updated type definition to an interface and added infoWindow

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,11 @@
+// Instructions to every other class on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -12,5 +20,23 @@ export class CustomMap {
         },
       }
     );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there",
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
   }
 }
