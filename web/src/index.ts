2aceb0c3423262ed25176448c13e6f315a7f7a6d
Cody Swithenbank <cody.swithenbank@gmail.com>
2023-02-10 18:15:55 -0500

    feat(web): project complete

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,35 @@
-import { User } from "./models/User";
+import { UserEdit } from "./views/UserEdit";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
 
-const user = User.buildUser({ id: 2 });
+const user = User.buildUser({ name: "NAME", age: 20 });
 
-user.on("change", () => {
-  console.log(user);
+const root = document.getElementById("root");
+
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+
+  console.log(userEdit);
+} else {
+  throw new Error("Root element not found");
+}
+
+// Generate the users
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on("change", () => {
+  const root = document.getElementById("collection");
+
+  if (root) {
+    new UserList(root, users).render();
+  }
 });
 
-user.fetch();
+users.fetch();
