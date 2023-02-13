7a7642862458ad21da9d47109874e3e0d7f27364
Cody Swithenbank <cody.swithenbank@gmail.com>
2023-02-13 17:04:36 -0500

    feat(server): setting up controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,10 @@ import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
+import { router as controllerRouter } from "./controllers/decorators/controller";
+
+import './controllers/LoginController'
+
 const app = express();
 
 // middleware
@@ -11,6 +15,7 @@ app.use(cookieSession({ keys: ["asdfsaf"] }));
 
 // routes
 app.use(router);
+app.use(controllerRouter)
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
